abf9d1a57b7cc30f6bec8850aac5ca5d1aaf2b01
AbstractUltra <dragonhe36@gmail.com>
2020-02-15 11:14:57 -0500

    i
    u

--- app/app/src/main/java/com/abstractultra/saferspace/MainActivity.kt
+++ app/app/src/main/java/com/abstractultra/saferspace/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.abstractultra.saferspace
 
 import android.Manifest
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import io.radar.sdk.Radar
-import kotlinx.android.synthetic.main.activity_main.*
+import io.radar.sdk.RadarTrackingOptions
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +18,19 @@ class MainActivity : AppCompatActivity() {
         Radar.userId = "AU"
         Radar.description = "Main User"
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1002)
+        /*
         Radar.trackOnce { status, location, events, user ->
             // do something with status, location, events, user
             textView.setText(location.toString())
         }
+        */
+        val trackingOptions : RadarTrackingOptions = RadarTrackingOptions.Builder()
+            .priority(Radar.RadarTrackingPriority.RESPONSIVENESS) // use EFFICIENCY instead to reduce location update frequency
+            .offline(Radar.RadarTrackingOffline.REPLAY_STOPPED) // use REPLAY_OFF instead to disable offline replay
+            .sync(Radar.RadarTrackingSync.ALL) // use ALL instead to sync all location updates
+            .build()
+        Radar.startTracking(trackingOptions)
+        
     }
 
 }
