8cd4750f4abf4e9f2062549510ed14a49aab26ad
AbstractUltra <dragonhe36@gmail.com>
2020-02-15 21:09:39 -0500

    uie
    oeieui

--- app/app/src/main/java/com/abstractultra/saferspace/MoreInfo.kt
+++ app/app/src/main/java/com/abstractultra/saferspace/MoreInfo.kt
@@ -1,32 +1,29 @@
 package com.abstractultra.saferspace
 
 import android.Manifest
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import io.radar.sdk.Radar
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_more_info.*
 
-
-class MainActivity : AppCompatActivity() {
+class MoreInfo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_more_info)
         Radar.initialize("prj_test_pk_65eb3283643d862e050466eadedd1eac6649c3d7")
         Radar.userId = "AU"
         Radar.description = "Main User"
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1002)
+        var latitude = ""
+        var longitude = ""
         Radar.trackOnce { status, location, events, user ->
             // do something with status, location, events, user
+            latitude = location?.latitude.toString()
+            longitude = location?.longitude.toString()
         }
         Radar.startTracking()
-        infobutton.setOnClickListener {
-            val myIntent = Intent(this@MainActivity, MoreInfo::class.java)
-            this@MainActivity.startActivity(myIntent)
-        }
+        location_text.setText("Your current location is 43.78676223754883, -79.1895980834961")
     }
-
 }
